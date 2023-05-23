cbff57a979c881418649de7dffb1d78e7f5ad708
Zai <zaiyellyintaung@gmail.com>
2023-05-24 01:53:48 +0630

    Before routes

---
+++ api/routes/freelanceRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/freelanceController");
+
+module.exports = router;
---
+++ api/routes/jobRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/jobController");
+
+module.exports = router;
