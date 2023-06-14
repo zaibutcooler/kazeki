dcc2c2b2555f34c9a0df23102b2a8c09cc6aa699
Zai <zaiyellyintaung@gmail.com>
2023-06-14 09:02:27 +0630

    Done routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getAll,
+  getOne,
+  createOne,
+  updateOne,
+  deleteOne,
+} = require("../controllers/todolistController");
+
+router.use(express.json());
+
+router.get("/", getAll);
+
+router.get("/:id", getOne);
+
+router.post("/", createOne);
+
+router.patch("/:id", updateOne);
+
+router.delete("/:id", deleteOne);
 
 module.exports = router;
