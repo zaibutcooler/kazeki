db0ab240ae8982c4a01f374075a4425b3f1b2cf0
Zai <zaiyellyintaung@gmail.com>
2023-07-10 23:41:36 +0630

    nextjs tesing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/userController");
-
-router.post("/register", register);
+const { login, register } = require("../controllers/userController");
 
 router.post("/login", login);
+
+router.post("/register", register);
 
 module.exports = router;
--- frontend/src/pages/client/CreateFreelanceOfferPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CreateFreelanceOfferPage = () => {
-  return <div>CreateFreelanceOfferPage</div>;
-};
-
-export default CreateFreelanceOfferPage;
