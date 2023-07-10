fae1de04d787685b558ef5074179a41b9613a63f
Zai <zaiyellyintaung@gmail.com>
2023-07-10 23:50:40 +0630

    left navbar

---
+++ next/app/_components/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <h2>Navbar</h2>;
+};
+
+export default Navbar;
--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "./_components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
