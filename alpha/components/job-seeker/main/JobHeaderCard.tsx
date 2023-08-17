367282d42842064cb879e337581b0365631dd314
Zai <zaiyellyintaung@gmail.com>
2023-08-17 23:46:14 +0630

    starting headers

--- alpha/components/job-seeker/main/JobHeaderCard.tsx
+++ alpha/components/job-seeker/main/JobHeaderCard.tsx
@@ -8,20 +8,29 @@ const JobHeaderCard = () => {
   const profile = session?.user.userProfile as ProfileType;
 
   return (
-    <div className="bg-white mb-4 p-4 rounded-lg shadow-md">
-      <div className="flex justify-between items-center">
-        <h1 className="text-xl font-semibold text-slate800">
-          {profile && profile.client
-            ? "Welcome back, Client!"
-            : "Welcome back, Seeker!"}
-        </h1>
-
-        {profile && profile.client && (
-          <Link
-            href="/client/job-offer"
-            className="bg-slate800 text-white px-4 py-2 rounded-md font-medium hover:bg-slate700">
-            Post a Job
-          </Link>
+    <div className="mt-3 mb-4 p-4 rounded-md border font-medium text-slate-800 w-full">
+      <div className="flex justify-between items-center w-full">
+        {session?.user ? (
+          <section className="w-full">
+            <h1 className="text-lg mb-3">
+              Welcome back{" "}
+              <span className="text-black">{profile.firstName}</span> !
+            </h1>
+            {profile.client ? (
+              <div className="flex gap-6 w-full text-sm">
+                <button className="w-full px-4 py-2 rounded-full border">
+                  Start Posting Your Job Offer ...
+                </button>
+                <button className="w-auto py-2 border px-4 rounded-md">
+                  Applications
+                </button>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </section>
+        ) : (
+          <section>Please login first.</section>
         )}
       </div>
     </div>
